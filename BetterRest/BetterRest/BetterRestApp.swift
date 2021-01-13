e751ef34a8cf496ad89dea85a030b44a07a7a880
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2021-01-13 19:53:10 +0000

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hubert Leszkiewicz on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

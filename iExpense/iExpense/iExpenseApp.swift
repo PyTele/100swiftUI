7c7e8708c67a7bf7b8895de720a71ee4c4465460
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2021-01-26 23:33:09 +0000

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hubert Leszkiewicz on 21/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

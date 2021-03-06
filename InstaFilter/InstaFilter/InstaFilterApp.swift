19929765ab38e65c98665102e724eb381e13f9df
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2021-03-06 22:25:31 +0000

    Add files via upload

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Hubert Leszkiewicz on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaFilter/InstaFilter/imageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  imageSaver.swift
+//  InstaFilter
+//
+//  Created by Hubert Leszkiewicz on 04/03/2021.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}

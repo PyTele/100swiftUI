3b658012b2a23b7bdc69a011d8d9ccad8abfa961
Hubert Leszkiewicz <64101280+zerotwoswift@users.noreply.github.com>
2021-04-07 23:05:54 +0100

    Add files via upload

---
+++ Hot prospects/Hot prospects/Hot_prospectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_prospectsApp.swift
+//  Hot prospects
+//
+//  Created by Hubert Leszkiewicz on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hot_prospectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hot prospects/Hot prospects/Prospect.swift
@@ -0,0 +1,28 @@
+//
+//  Prospect.swift
+//  Hot prospects
+//
+//  Created by Hubert Leszkiewicz on 27/03/2021.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+    }
+}
